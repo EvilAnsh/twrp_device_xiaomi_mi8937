115dc5772fd2e9c714601173c34d13c90baf0cbd
Ansh <singhansh64321@gmail.com>
2022-06-17 09:52:49 +0000

    mi8937: Update Vars
    *Included addons
    *Changed Maintainer name
    *Added specific Magisk zip
    *Added avatar
    
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- BoardConfigOFOX.mk
+++ BoardConfigOFOX.mk
@@ -2,4 +2,6 @@
 OF_ALLOW_DISABLE_NAVBAR := 1
 OF_NO_TREBLE_COMPATIBILITY_CHECK := 1
 OF_USE_TWRP_SAR_DETECT := 1
-OF_MAINTAINER := ꪖ​ꪀ𝘴ꫝ|邪惡的
+OF_MAINTAINER := EvilAnsh
+OF_MAINTAINER_AVATAR="/home2/evilansh07/fox_11.0/device/xiaomi/mi8937/addon/avatar.png"
+FOX_USE_SPECIFIC_MAGISK_ZIP="/home2/evilansh07/fox_11.0/device/xiaomi/mi8937/addon/Magisk-24.3.zip"
